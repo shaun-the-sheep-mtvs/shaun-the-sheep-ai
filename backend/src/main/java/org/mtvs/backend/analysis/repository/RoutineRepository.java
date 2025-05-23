1bf521a08725d6111438b0447d26823e909a6b9b
nkokua <zy013@naver.com>
2025-05-23 14:16:21 +0900

    temp delete analysis

--- backend/src/main/java/org/mtvs/backend/analysis/repository/RoutineRepository.java
+++
@@ -1,7 +0,0 @@
-package org.mtvs.backend.analysis.repository;
-
-import org.mtvs.backend.analysis.domain.Routine;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoutineRepository extends JpaRepository<Routine, Integer> {
-}
