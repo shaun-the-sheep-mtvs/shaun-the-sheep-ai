d8fc9cee4e1755ac1d2e97bee36e4686a75f4f44
kjs9504 <emflazlwm62@gmail.com>
2025-05-24 21:44:30 +0900

    dev 합치기

--- backend/src/main/java/org/mtvs/backend/auth/config/SecurityConfig.java
+++ backend/src/main/java/org/mtvs/backend/auth/config/SecurityConfig.java
@@ -32,6 +32,7 @@ public class SecurityConfig {
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
