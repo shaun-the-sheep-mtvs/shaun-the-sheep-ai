65dd1d756abc21f33ef0d8201ab2aa7bfcee3713
yunnzi040 <zeongyunzi@gmail.com>
2025-05-26 17:31:41 +0900

    feat: 사용자별 맞춤형 제품 추천 시스템 구현

--- backend/src/main/java/org/mtvs/backend/product/repository/ProductRepository.java
+++ backend/src/main/java/org/mtvs/backend/product/repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package org.mtvs.backend.product.repository;
 
 import org.mtvs.backend.product.entity.Product;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -10,4 +11,5 @@ public interface ProductRepository extends JpaRepository<Product, String> {
     List<Product> findByUserId(String id);
     boolean existsByUserId(String id);
 
+    List<Product> findByUserIdAndFormulationType(String userId, String formulationType);
 }
