3482d7abb5332196bb9e5c710f1f0077cd1816d2
kjs9504 <emflazlwm62@gmail.com>
2025-05-26 10:00:50 +0900

    dev 합침

--- backend/src/main/java/org/mtvs/backend/chat/config/WebClientConfig.java
+++ backend/src/main/java/org/mtvs/backend/chat/config/WebClientConfig.java
@@ -11,7 +11,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
-    @Value("${gemini.api.key}")
+    @Value("${GEMINI_API_KEY}")
     private String apiKey;
 
     @Bean
