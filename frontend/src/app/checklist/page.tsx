ba69c24ac35755c43eafda63d36dc640e253f2ef
jake <jktom@gmail.com>
2025-06-04 17:11:26 +0900

    test jenkins

--- frontend/src/app/checklist/page.tsx
+++ frontend/src/app/checklist/page.tsx
@@ -6,8 +6,8 @@ import { CONCERNS } from '@/data/concerns';
 import styles from './page.checklist.module.css';
 import { ArrowLeft, RotateCcw } from 'lucide-react';
 import { useRouter } from 'next/navigation';
-import Navbar from '@/components/Navbar';
-import { apiConfig } from '@/config/api';
+import Navbar from "@/components/Navbar"
+import apiConfig from '@/config/api'
 
 // Fisher–Yates 셔플 함수 추가
 function shuffle<T>(arr: T[]): T[] {
